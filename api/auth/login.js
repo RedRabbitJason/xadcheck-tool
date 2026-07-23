e889be8249b694c57a078e02892f89fa18e3822e
RedRabbitJason <80135988+RedRabbitJason@users.noreply.github.com>
2026-07-23 00:04:09 -0700

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const clientId = "QkFZS3JTdlc4NTk2VjN2SzR6SVg6MTpjaQ";
-  const redirectUri = "https://xadcheck-tool.vercel.app/api/auth/callback";
+  const redirectUri = "https://xadcheck-tool.vercel.app/api/auth-callback";
   const scope = "users.read tweet.read offline.access";
 
   const authUrl = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&state=state&code_challenge=challenge&code_challenge_method=plain`;
